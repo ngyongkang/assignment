33e7bde0c0034ab66cd355e16d0cb24966122f85
ngyongkang <81545663+ngyongkang@users.noreply.github.com>
2023-05-07 22:46:48 +0800

    Add the table to the frontend & add docker files.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # assignment
+To start the application, use the docker command below.
+docker compose up -d
+To stop the application and remove all images use the docker command below.
+docker compose down --rmi all
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,28 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import { DataGrid } from '@mui/x-data-grid';
-
+import styled from "@emotion/styled";
+import { TextField } from "@mui/material";
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 90 },
   { field: 'name', headerName: 'Name', width: 130 },
   { field: 'login', headerName: 'Login', width: 130 },
-  { field: 'salary', headerName: 'Salary', type: 'double',width: 130 },
-  // {
-  //   field: 'age',
-  //   headerName: 'Age',
-  //   type: 'number',
-  //   width: 90,
-  // },
-  // {
-  //   field: 'fullName',
-  //   headerName: 'Full name',
-  //   description: 'This column has a value getter and is not sortable.',
-  //   sortable: false,
-  //   width: 160,
-  //   valueGetter: (params) =>
-  //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  // },
+  { field: 'salary', headerName: 'Salary', type: 'double',width: 130 }
 ];
+
+const StyledTextField = styled(TextField)`
+	margin: 10px 0;
+	width: 30%;
+`;
+
+
 
 function App() {
   const [users, setUsers] = useState([]);
+  // const [searchWord, setSearchWord] = useState("");
+  const [minSalary, setMinSalary] = useState("");
+  const [maxSalary, setMaxSalary] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/users")
@@ -34,29 +30,64 @@ function App() {
       .then((data) => setUsers(data.data))
       .catch(err => console.log(err));
   }, []);
-  
-  // return (
-  //   <div className="App">
-  //     {users.map((list, index) => (
-  //       <li key={index}>{list.id} | {list.name}</li>
-  //     ))}
-  //   </div>
-  // );
+
+  // const globalSearch = () => {
+	// 	const filteredRepositories = users.filter((value) => {
+	// 		return (
+	// 			value?.id?.toLowerCase().includes(searchWord?.toLowerCase()) ||
+  //       value?.name?.toLowerCase().includes(searchWord?.toLowerCase()) ||
+	// 			value?.login?.toLowerCase().includes(searchWord?.toLowerCase()) ||
+	// 			value?.salary?.toString().includes(searchWord?.toLowerCase())
+	// 		);
+	// 	});
+	// 	return filteredRepositories;
+	// };
+
+  const salarySearch = () => {
+		const filteredRepositories = users.filter((value) => {
+			return (
+				parseFloat(value?.salary?.toString()) >= parseFloat(minSalary?.toLowerCase()) &&
+        parseFloat(value?.salary?.toString()) <= parseFloat(maxSalary?.toLowerCase())
+			);
+		});
+		return filteredRepositories;
+	};
+
+  const filterRepositoryList = minSalary && maxSalary ? salarySearch() : users;
 
   return (
-    <div style={{ height: 430, width: '100%', 'margin-right': '10px' ,'marign-left': 'right' }}>
-      <h1>Employees</h1>
-      <DataGrid
-        rows={users}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 6 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
+    <div style={{ height: '100%', width: '80%', margin: '0% 10% 0% 10%'}}>
+      <div style={{width: '100%', display: 'flex', float: 'left'}}>
+        <StyledTextField
+          value={minSalary}
+          onChange={(event) => setMinSalary(event.target.value)}
+          label="Minimum Salary"
+          variant="outlined"
+        />
+        <p style={{margin: '25px 3% 25px 3%'}}>-</p>
+        <StyledTextField
+          value={maxSalary}
+          onChange={(event) => setMaxSalary(event.target.value)}
+          label="Maximum Salary"
+          variant="outlined"
+        />
+      </div>
+      <div>
+        <h1>Employees</h1>
+      </div>
+      <div>
+        <DataGrid 
+          rows={filterRepositoryList && filterRepositoryList}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 6 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          // checkboxSelection
+        />
+      </div>
   </div>
   );
 }
